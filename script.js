18082b5dd3f3b23956963961f164fcc29baf3ac2
Benjamin <158299275+BenjaminAlbert387@users.noreply.github.com>
2025-04-18 20:12:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+// Set the date we're counting down to
+var countDownDate = new Date("May 27, 2025 10:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
